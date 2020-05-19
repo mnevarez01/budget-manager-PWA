6ea40760ef4983cc2114c19ac01dff68a0137985
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-18 21:50:43 -0600

    added public folder

---
+++ public/service-worker.js
@@ -0,0 +1,53 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone())
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => { console.log(err) })
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then(response => {
+        if (response) {
+          return response
+        } else if (event.request.headers.get("accept").include("text/html")) {
+          return caches.match("/")
+        }
+      })
+    })
+  )
+})
